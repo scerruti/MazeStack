46155276ad75cb089b1ba529b9fcaf1e47b21491
scerruti <scerruti@otabi.com>
2014-09-09 15:28:14 -0700

    Navigator solution

--- src/com/otabi/iaroc/maze/Navigator.java
+++ src/com/otabi/iaroc/maze/Navigator.java
@@ -7,35 +7,117 @@ import java.util.Stack;
  */
 public class Navigator {
 	private Stack<Character> moves;
-	
+
+	private enum Mode {
+		Mapping, Backtracking, Solving
+	};
+
+	Mode mode = Mode.Mapping;
+
 	public Navigator() {
 		moves = new Stack<Character>();
 	}
-	
+
 	public void recordMove(char move) {
-		System.out.print(move);
+		if (mode == Mode.Solving) return;
+		
+		System.out.print((mode == Mode.Mapping?move:Character.toLowerCase(move)));
 		System.out.print('-');
-		moves.push(move);
+		
+		// Record Move
+		if (mode == Mode.Mapping) {
+			if (move != 'U') {
+				moves.push(move);
+			} else {
+				mode = Mode.Backtracking;
+			}
+		} 
+		
+		
+		else if (mode == Mode.Backtracking) {
+			
+			// If stack is empty, start over
+			if (moves.empty()) {
+				switch (move) {
+				case 'R':
+					moves.push('L');
+					break;
+				case 'L':
+					moves.push('R');
+					break;
+				default:
+					System.err.println("This shouldn't have happened!");
+				}
+				mode = Mode.Mapping;
+			}
+			
+			// Are we still backtracking
+			else {
+				Character nextMove = moves.pop();
+			
+				if (!isBacktrackMove(move, nextMove)) {
+					switch (move) {
+					case 'N':
+						// Step onto new path (replace a turn with a turn/move)
+						if (nextMove == 'R') {
+							moves.push('L');
+						}
+						else if (nextMove == 'L') {
+							moves.push('R');
+						} 
+						break;
+					case 'L':
+						if (nextMove == 'L') {
+							// If we previously took a left and want to take another left, that means straight through
+							moves.push('N');
+						}
+						else if (nextMove == 'N') {
+							// If we previously moved forward, then we need to keep that move and add the turn (remember to reverse it)
+							moves.push('R');
+						}
+						break;
+					case 'R':
+						// Reverse 'L'
+						if (nextMove == 'R') {
+							moves.push('N');
+						}
+						else if (nextMove == 'N') {
+							moves.push('L');
+						}
+						break;
+					}
+					mode = Mode.Mapping;
+				}
+			}
+		}
+	}
+
+	private boolean isBacktrackMove(char move, Character nextMove) {
+		char next = (nextMove=='R'?'L':(nextMove=='L'?'R':nextMove));  // Switch left and right
+		return (move == next);
 	}
 
 	public Character getNextMove() {
-		if (moves.isEmpty()) return null;
-		
+		if (moves.isEmpty())
+			return null;
+
 		char nextMove = moves.pop();
-		
+
 		if (nextMove == 'R') {
 			nextMove = 'L';
-		} else if (nextMove == 'L'){
+		} else if (nextMove == 'L') {
 			nextMove = 'R';
 		}
-		
+
 		return (new Character(nextMove));
 	}
-	
+
 	public String returnPath() {
+		mode = Mode.Solving;
 		System.out.println();
 		StringBuilder path = new StringBuilder();
-		Character nextMove;;
+		Character nextMove;
+		;
 		while ((nextMove = getNextMove()) != null) {
 			path.append(nextMove);
 			System.out.print(nextMove);
